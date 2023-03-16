640fec076c29ec17fb500272eece40bab37ba4c1
Darya <iontseva.dasha>
2023-03-16 21:29:28 +0300

    task1

--- Task1.py
+++ Task1.py
@@ -5,8 +5,8 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0) 
 
-volue = int(input('Введите трёхзначное число: '))
-number1 = volue % 10
-number2 = volue % 100 // 10
-number3 = volue // 100
-print(number1 + number2 + number3)
+number = int(input('Введите трёхзначное число: '))
+digit1 = number % 10
+digit2 = number % 100 // 10
+digit3 = number // 100
+print(digit1 + digit2 + digit3)
